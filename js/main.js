f1f6093d46ce33baca845609ab4dc0dc747fc7f5
Vladimir Tiunov <tiunov2008.va@gmail.ru>
2021-05-31 20:03:58 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
-var money = 1000;
-var income = "Some text";
-var addExpenses = 'Text, More_text, Text';
-var deposit = true;
-var mission = 100000;
-var period = 6;
-var budgetDay = 30;
-var num = 266219;
-num = String(num).split("").reduce((last, next) => last * next)**3;
-console.log(String(num).substr(0,2));
+'use strict'
 
-alert("Some text");
-console.log("Some text");
-console.log("Type of money: " + typeof(money));
-console.log("Type of income: " + typeof(income));
-console.log("Type of deposit: " + typeof(deposit));
-console.log("Length of addExpenses: " + addExpenses.length);
+let money = prompt('Ваш месячный доход?');
+let income = 'Some text';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?');
+let amount2 = prompt('Во сколько это обойдется?');
+let mission = 100000;
+let budgetMonth = money - amount1 - amount2;
+let budgetDay = budgetMonth / 30;
+let num = 266219;
+let period = mission / budgetMonth;
+
+
+
+/*num = String(num).split('').reduce((last, next) => last * next)**3;
+console.log(String(num) .substr(0,2));*/
+
+alert('Some text');
+console.log('addExpenses: ' + addExpenses.length);
 console.log(addExpenses.toLowerCase().split(', '));
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей");
-console.log("budgetDay: " + 30);
+console.log('Цель буде достигнута через ' + Math.ceil(period) + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log('Буджет на день: ' + budgetDay);
+console.log('budgetMonth: ' + budgetMonth);
+if (budgetDay>1200) {
+    console.log('У вас высокий уровень дохода')
+} else {
+    
+}
