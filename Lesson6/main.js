4ed6fdbe9cf79c47f28f7e5a42d5069a7b7760b3
tiunov2008 <tiunov2008.va@gmail.ru>
2021-06-03 20:01:10 +0300

    ExtraTask

--- Lesson6/main.js
+++ Lesson6/main.js
@@ -2,24 +2,39 @@
 
 let answer = Math.floor(Math.random() * 100) + 1;
 console.log(answer);
+
 let start = function(){
     let number = prompt('Угадай число от 1 до 100');
+    let attempts = 10;
+
     let checkAnswer = function(){
         if(number === null){
             alert('Игра окончена');
+            return
+        }else if(attempts === 0){
+            let message2 = confirm('Попытки закончились, хотите сыграть еще?');
+            if(message2 === true){
+                start();
+            }else{
+                alert('Игра окончена');
+                return
+            }
         }else if(!isNaN(parseFloat(number)) && isFinite(number)){
             if(answer == number){
-                let message = confirm('Поздравляю, Вы угадали!!!');
-                if(message === true){
+                let message1 = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+                if(message1 === true){
                     start();
                 }else{
-
+                    alert('Игра окончена');
+                    return
                 }
             }else if(answer < number){
-                number = prompt('Загаданное число меньше');
+                number = prompt('Загаданное число меньше, осталось попыток ' + attempts);
+                attempts--;
                 checkAnswer();
             }else if(answer > number){
-                number = prompt('Загаданное число больше');
+                number = prompt('Загаданное число больше, осталось попыток ' + attempts);
+                attempts--;
                 checkAnswer();
             }
             checkAnswer();
@@ -28,7 +43,9 @@ let start = function(){
             checkAnswer();
         }
     }
+    console.dir(checkAnswer);
     checkAnswer();
+
 }
 start();
 
