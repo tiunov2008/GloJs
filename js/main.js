92fc0aab45452a325af77ecbd89428c9544026b2
tiunov2008 <tiunov2008.va@gmail.ru>
2021-06-05 09:20:48 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@ let appData = {
     budgetDay: 0,
     budgetMonth: 0,
     expensesMonth: 0,
-    accumulatedMonth: 0,
     asking: function(){
         appData.addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую','Квартплата, проездной, кредит');//Возможные расходы
         appData.addExpenses.toLowerCase().split(',');
@@ -52,9 +51,8 @@ let appData = {
         }
     },    
     getExpensesMonth: function(){
-        let i;
         for(let key in appData.expenses){
-            appData.expensesMonth += appData.expenses[i];
+            appData.expensesMonth += +appData.expenses[key];
         }
         
         if(!(appData.expensesMonth >= 0)){
@@ -82,7 +80,7 @@ let appData = {
 }
 appData.asking();
 appData.getBudget();
-
+appData.getExpensesMonth();
 console.log('Расходы за месяц: ' + appData.expensesMonth);
 
 console.log(appData.getTargetMonth());
@@ -91,4 +89,4 @@ appData.getStatusIncome();
 console.log("Наша программа включает в себя данные:");
 for(let key in appData){
     console.log(key + ":  " + appData[key]);
-}
+} 
