b3709ea3da3b3e9247f137ca7993e59ce6146b3d
tiunov2008 <tiunov2008.va@gmail.ru>
2021-06-08 20:42:03 +0300

    fix

--- Extralesson6/main.js
+++ Extralesson6/main.js
@@ -4,11 +4,11 @@ let weekHtml = document.querySelector('.week');
 let week = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'];
 for (let i = 0; i < week.length; i++) {
     if((i == 0 || i == 6) && i != date.getDay()){
-        weekHtml.append(week[i] + '  :   Выходной'); 
+        weekHtml.innerHTML += '<i>' + week[i] + '</i>' + '<br>'; 
     }else if(i == date.getDay()){
-        console.log(week[i] + '  :   Сегодня'); 
+        weekHtml.innerHTML += '<b>' + week[i] + '</b>' + '<br>'; 
     }else{
-        console.log(week[i]); 
+        weekHtml.innerHTML += week[i] + '<br>'; 
     }   
 }
 
