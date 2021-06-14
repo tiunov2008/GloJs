2ffb746b3047719d0c4c925ab3fc592baf7cfa23
tiunov2008 <tiunov2008.va@gmail.ru>
2021-06-14 21:18:41 +0300

    ExtraLesson

--- Lesson14/js/main.js
+++ Lesson14/js/main.js
@@ -1,34 +1,65 @@
 'use strict'
-
-const AppData = function () {
-    this.selector = '.class';
-    this.height = 100;
-    this.width = 1000;
-    this.bg = '#000fff';
-    this.fontSize = 100;
-}
-AppData.prototype.createElem = function(){
-    if(this.selector[0] === '.'){
-        let div = document.createElement('div');
-        div.className = this.selector.substr(1).trim();
-        div.innerHTML = 'Я div';
-        document.body.append(div);
-    }else if(this.selector[0] === '#'){
-        let p = document.createElement('p');
-        p.id = this.selector.substr(1).trim();
-        p.innerHTML = 'Я p';
-        document.body.append(p);
+//document.addEventListener("DOMContentLoaded", function () {
+    const AppData = function () {
+        this.selector = '.class';
+        this.height = 100;
+        this.width = 100;
+        this.bg = '#000fff';
+        this.fontSize = 100;
     }
-};
+    
+    AppData.prototype.createElem = function(){
+        if(this.selector[0] === '.'){
+            let div = document.createElement('div');
+            div.className = this.selector.substr(1).trim();
+            div.innerHTML = 'div';
+            document.body.append(div);
+        }else if(this.selector[0] === '#'){
+            let p = document.createElement('p');
+            p.id = this.selector.substr(1).trim();
+            p.innerHTML = 'p';
+            document.body.append(p);
+        }
+    };
 
-AppData.prototype.addStyles = function(){
-    let elem = document.querySelector(this.selector);
-    elem.style.height = this.height + 'px';
-    elem.style.width = this.width + 'px';
-    elem.style.background = this.bg;
-    elem.style.fontSize = this.fontSize + 'px';
-};
-let appData = new AppData();
+    AppData.prototype.addStyles = function(){
+        let elem = document.querySelector(this.selector);
+        elem.style.height = this.height + 'px';
+        elem.style.width = this.width + 'px';
+        elem.style.background = this.bg;
+        elem.style.fontSize = this.fontSize + 'px';
+        elem.style.position = 'absolute';
+    };
+    AppData.prototype.keydown = function(){
+        let elem = document.querySelector(this.selector);
+        let Y = 0;
+        let X = 0;
+        document.addEventListener('keydown', function(event) {
+            if (event.code == 'ArrowUp') {
+                Y -= 10;
+                elem.style.top = Y + 'px';
+                console.log(Y);
+            }
+            if (event.code == 'ArrowDown') {
+                Y += 10;
+                elem.style.top = Y + 'px';
+                console.log(Y);
+            }
+            if (event.code == 'ArrowLeft') {
+                X -= 10;
+                elem.style.left = X + 'px';
+                console.log(X);
+            }
+            if (event.code == 'ArrowRight') {
+                X += 10;
+                elem.style.left = X + 'px';
+                console.log(X);
+            }
+        });
+    };
 
-appData.createElem();
-appData.addStyles();
+    let appData = new AppData();
+    appData.createElem();
+    appData.addStyles();
+    appData.keydown();
+//});
