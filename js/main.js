46bf9edf8d296b271fee6fa5afa71ca819b7f9b2
Vova <tiunov2008.va@gmail.com>
2021-06-22 13:29:15 +0300

    first

--- js/main.js
+++ js/main.js
@@ -183,4 +183,101 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     tabs();
+
+    //слайдер
+
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item');
+        let dot = document.querySelectorAll('.dot');
+        const slider = document.querySelector('.portfolio-content');
+
+
+        let currentSlide = 0,
+            interval;
+        const createDots = () => {
+            for (let i = 0; i < slide.length; i++) {
+                const newDot = document.createElement('li');
+                newDot.classList.add('dot');
+                if (i === 0) {
+                    newDot.classList.add('dot-active');
+                }
+                document.querySelector('.portfolio-dots').append(newDot);
+            }
+        };
+        createDots();
+        dot = document.querySelectorAll('.dot');
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+        };
+
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide++;
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+
+        const startSlide = time => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+
+        slider.addEventListener('click', event => {
+            event.preventDefault();
+
+            const target = event.target;
+
+            if (!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            if (target.matches('#arrow-right')) {
+                currentSlide++;
+            } else if (target.matches('#arrow-left')) {
+                currentSlide--;
+            } else if (target.matches('.dot')) {
+                dot.forEach((elem, index) => {
+                    if (elem === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+
+            if (currentSlide < 0) {
+                currentSlide = slide.length - 1;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        });
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+
+        slider.addEventListener('mouseover', event => {
+            if (event.target.matches('.portfolio-btn') ||
+            event.target.matches('.dot')) {
+                stopSlide();
+            }
+        });
+
+        slider.addEventListener('mouseout', event => {
+            if (event.target.matches('.portfolio-btn') ||
+            event.target.matches('.dot')) {
+                startSlide(1500);
+            }
+        });
+        startSlide(1500);
+    };
+    slider();
 });
