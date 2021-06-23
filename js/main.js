fde320b5bd5ee35966f69b447428904dfb705f21
Vova <tiunov2008.va@gmail.com>
2021-06-23 16:06:06 +0300

    first

--- js/main.js
+++ js/main.js
@@ -280,4 +280,68 @@ window.addEventListener('DOMContentLoaded', () => {
         startSlide(1500);
     };
     slider();
+
+    const calculator = () => {
+        const items = document.querySelectorAll('input.calc-item');
+        items.forEach(elem => {
+            elem.addEventListener('input', () => {
+                elem.value = elem.value.replace(/\D/g, '');
+            });
+        });
+    };
+    calculator();
+
+    const questions = () => {
+        function uCaseFirst(s) {
+            return s.toLowerCase().replace(/(?<!\p{Lowercase})\p{Lowercase}/gu, ch => ch.toUpperCase());
+        }
+        const phone = document.getElementById('form2-phone'),
+            name = document.getElementById('form2-name'),
+            email = document.getElementById('form2-email'),
+            message = document.getElementById('form2-message');
+        phone.addEventListener('input', () => {
+            phone.value = phone.value.replace(/[^0-9()-]/, '');
+        });
+        name.addEventListener('input', () => {
+            name.value = name.value.replace(/[^а-яё -]/ig, '');
+        });
+        email.addEventListener('input', () => {
+            email.value = email.value.replace(/[^a-z@_.!~*'-]/ig, '');
+        });
+        message.addEventListener('input', () => {
+            message.value = message.value.replace(/[^а-яё -]/ig, '');
+        });
+        const blur = item => {
+            item.value = item.value.trim().replace(/^\-/, '').replace(/ {1,}/g, " ").replace(/\-{1,}/g, "-");
+        };
+        phone.addEventListener('focusout', () => {
+            blur(phone);
+        });
+        name.addEventListener('focusout', () => {
+            blur(name);
+            name.value = uCaseFirst(name.value);
+        });
+        email.addEventListener('focusout', () => {
+            blur(email);
+        });
+        message.addEventListener('focusout', () => {
+            blur(message);
+        });
+    };
+    questions();
+
+    const ourTeam = () => {
+        const photos = document.querySelectorAll('.command__photo');
+        photos.forEach(elem => {
+            let oldSrc;
+            elem.addEventListener('mouseenter', event => {
+                oldSrc = event.target.src;
+                event.target.src = event.target.dataset.img;
+            });
+            elem.addEventListener('mouseout', event => {
+                event.target.src = oldSrc;
+            });
+        });
+    };
+    ourTeam();
 });
