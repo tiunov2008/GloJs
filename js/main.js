ee0c3da7b90f8b4b23347dbba532dacb18fe28b4
Vladimir Tiunov <tiunov2008.va@gmail.ru>
2021-06-28 20:12:09 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -361,6 +361,89 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     calc(100);
 
+    const blurForms = () => {
+        function uCaseFirst(s) {
+            return s.toLowerCase().replace(/(?<!\p{Lowercase})\p{Lowercase}/gu, ch => ch.toUpperCase());
+        }
+        const phone2 = document.getElementById('form1-phone'),
+            name2 = document.getElementById('form1-name'),
+            email2 = document.getElementById('form1-email'),
+            phone = document.getElementById('form2-phone'),
+            name = document.getElementById('form2-name'),
+            email = document.getElementById('form2-email'),
+            message = document.getElementById('form2-message'), 
+            phone3 = document.getElementById('form3-phone'),
+            name3 = document.getElementById('form3-name'),
+            email3 = document.getElementById('form3-email');
+        phone.addEventListener('input', () => {
+            phone.value = phone.value.replace(/[^0-9()+-]/, '');
+        });
+        name.addEventListener('input', () => {
+            name.value = name.value.replace(/[^а-яё -]/ig, '');
+        });
+        email.addEventListener('input', () => {
+            email.value = email.value.replace(/[^a-z@_.!~*'-]/ig, '');
+        });
+        phone2.addEventListener('input', () => {
+            phone2.value = phone2.value.replace(/[^0-9()+-]/, '');
+        });
+        name2.addEventListener('input', () => {
+            name2.value = name2.value.replace(/[^а-яё -]/ig, '');
+        });
+        email2.addEventListener('input', () => {
+            email2.value = email2.value.replace(/[^a-z@_.!~*'-]/ig, '');
+        });
+        phone3.addEventListener('input', () => {
+            phone3.value = phone3.value.replace(/[^0-9()+-]/, '');
+        });
+        name3.addEventListener('input', () => {
+            name3.value = name3.value.replace(/[^а-яё -]/ig, '');
+        });
+        email3.addEventListener('input', () => {
+            email3.value = email3.value.replace(/[^a-z@_.!~*'-]/ig, '');
+        });
+        message.addEventListener('input', () => {
+            message.value = message.value.replace(/[^а-яё -]/ig, '');
+        });
+        const blur = item => {
+            item.value = item.value.trim().replace(/^\-/, '').replace(/ {1,}/g, " ").replace(/\-{1,}/g, "-").replace(/\+{1,}/g, "+");
+        };
+        phone.addEventListener('focusout', () => {
+            blur(phone);
+        });
+        name.addEventListener('focusout', () => {
+            blur(name);
+            name.value = uCaseFirst(name.value);
+        });
+        email.addEventListener('focusout', () => {
+            blur(email);
+        });
+        phone2.addEventListener('focusout', () => {
+            blur(phone2);
+        });
+        name2.addEventListener('focusout', () => {
+            blur(name2);
+            name2.value = uCaseFirst(name2.value);
+        });
+        email2.addEventListener('focusout', () => {
+            blur(email2);
+        });
+        phone3.addEventListener('focusout', () => {
+            blur(phone3);
+        });
+        name3.addEventListener('focusout', () => {
+            blur(name3);
+            name3.value = uCaseFirst(name3.value);
+        });
+        email3.addEventListener('focusout', () => {
+            blur(email3);
+        });
+        message.addEventListener('focusout', () => {
+            blur(message);
+        });
+    };
+    blurForms();
+
     const questions = () => {
         function uCaseFirst(s) {
             return s.toLowerCase().replace(/(?<!\p{Lowercase})\p{Lowercase}/gu, ch => ch.toUpperCase());
@@ -370,7 +453,7 @@ window.addEventListener('DOMContentLoaded', () => {
             email = document.getElementById('form2-email'),
             message = document.getElementById('form2-message');
         phone.addEventListener('input', () => {
-            phone.value = phone.value.replace(/[^0-9()-]/, '');
+            phone.value = phone.value.replace(/[^0-9()+-]/, '');
         });
         name.addEventListener('input', () => {
             name.value = name.value.replace(/[^а-яё -]/ig, '');
@@ -382,7 +465,7 @@ window.addEventListener('DOMContentLoaded', () => {
             message.value = message.value.replace(/[^а-яё -]/ig, '');
         });
         const blur = item => {
-            item.value = item.value.trim().replace(/^\-/, '').replace(/ {1,}/g, " ").replace(/\-{1,}/g, "-");
+            item.value = item.value.trim().replace(/^\-/, '').replace(/ {1,}/g, " ").replace(/\-{1,}/g, "-").replace(/\+{1,}/g, "+");
         };
         phone.addEventListener('focusout', () => {
             blur(phone);
@@ -414,4 +497,95 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     ourTeam();
+
+    //send-ajax-form
+
+    
+    const sendForm = () => {
+        const errorMessage = 'Что то пошло не так...',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро с вами свяжимся!';
+
+        const form =  document.getElementById('form1');
+        const form2 =  document.getElementById('form2');
+        const form3 =  document.getElementById('form3');
+
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = 'font-size: 2rem; color: #FFF !important;';
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            const formData = new FormData(form);
+            let body = {};
+            for (let val of formData.entries()){
+                body[val[0]] = val[1];
+            }
+            postData(body, 
+                () => {
+                    statusMessage.textContent = successMessage;
+                }, 
+                (error) => {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+            }, form);
+        });
+        form2.addEventListener('submit', event => {
+            event.preventDefault();
+            form2.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            const formData = new FormData(form2);
+            let body = {};
+            for (let val of formData.entries()){
+                body[val[0]] = val[1];
+            }
+            console.log(body);
+            postData(body, 
+                () => {
+                    statusMessage.textContent = successMessage;
+                }, 
+                (error) => {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+            }, form2);
+        });
+        form3.addEventListener('submit', event => {
+            event.preventDefault();
+            form3.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            const formData = new FormData(form3);
+            let body = {};
+            for (let val of formData.entries()){
+                body[val[0]] = val[1];
+            }
+            postData(body, 
+                () => {
+                    statusMessage.textContent = successMessage;
+                }, 
+                (error) => {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+            }, form3);
+        });
+    };
+    const postData = (body, outputData, errortData, form) => {
+        const request = new XMLHttpRequest;
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) {
+                return;
+            }
+            if (request.status === 200) {
+                outputData();
+            } else {
+                errortData(request.status);
+            }
+        });
+
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(body));
+        form.reset();
+    };
+    sendForm();
 });
