66b599ba0af7b44add2753374fe7738a04108e8d
Vladimir Tiunov <tiunov2008.va@gmail.com>
2021-07-09 08:54:03 +0300

    Lesson29

--- Lesson34/main.js
+++ Lesson34/main.js
@@ -22,6 +22,8 @@ canvas.addEventListener('mousemove', event => {
         y = event.offsetY,
         mx = event.movementX,
         my = event.movementY;
+    console.log(x);
+    console.log(y);
     if (event.buttons > 0) {
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -32,6 +34,28 @@ canvas.addEventListener('mousemove', event => {
 });
 ctx.beginPath();
 ctx.lineWidth = 5;
+ctx.strokeStyle = 'yellow';
+ctx.moveTo(canvas.width - 112.5, canvas.height - 75);
+ctx.arc(canvas.width - 137.5, canvas.height - 75, 25, 0, angle(360), false);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(canvas.width - 20, canvas.height - 100);
+ctx.arc(canvas.width - 45, canvas.height - 100, 25, 0, angle(360), false);
+ctx.stroke();
+ctx.strokeStyle = '#000000';
+ctx.closePath();
+
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(canvas.width - 52.5, canvas.height - 75);
+ctx.arc(canvas.width - 77.5, canvas.height - 75, 25, 0, angle(360), false);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
 ctx.strokeStyle = 'blue';
 ctx.moveTo(canvas.width - 140, canvas.height - 100);
 ctx.arc(canvas.width - 165, canvas.height - 100, 25, 0, angle(360), false);
@@ -40,30 +64,47 @@ ctx.closePath();
 
 ctx.beginPath();
 ctx.strokeStyle = '#000000';
-ctx.moveTo(canvas.width - 85, canvas.height - 100);
-ctx.arc(canvas.width - 110, canvas.height - 100, 25, 0, angle(360), false);
+ctx.moveTo(canvas.width - 80, canvas.height - 100);
+ctx.arc(canvas.width - 105, canvas.height - 100, 25, 0, angle(360), false);
 ctx.stroke();
 ctx.closePath();
 
+
 ctx.beginPath();
-ctx.strokeStyle = 'red';
-ctx.moveTo(canvas.width - 30, canvas.height - 100);
-ctx.arc(canvas.width - 55, canvas.height - 100, 25, 0, angle(360), false);
+ctx.strokeStyle = 'yellow';
+ctx.moveTo(canvas.width - 112.5, canvas.height - 75);
+ctx.arc(canvas.width - 137.5, canvas.height - 75, 25, 0, angle(256), false);
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
 ctx.strokeStyle = 'yellow';
 ctx.moveTo(canvas.width - 112.5, canvas.height - 75);
-ctx.arc(canvas.width - 137.5, canvas.height - 75, 25, 0, angle(360), false);
+ctx.arc(canvas.width - 137.5, canvas.height - 75, 25, 0, angle(280), true);
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.strokeStyle = 'green';
-ctx.moveTo(canvas.width - 57.5, canvas.height - 75);
-ctx.arc(canvas.width - 82.5, canvas.height - 75, 25, 0, angle(360), false);
+ctx.strokeStyle = '#000000';
+ctx.moveTo(canvas.width - 80, canvas.height - 100);
+ctx.arc(canvas.width - 105, canvas.height - 100, 25, 0, angle(140), false);
 ctx.stroke();
-ctx.strokeStyle = '#000000  ';
 ctx.closePath();
 
+
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(canvas.width - 52.5, canvas.height - 75);
+ctx.arc(canvas.width - 77.5, canvas.height - 75, 25, 0, angle(200), false);
+ctx.stroke();
+ctx.closePath();
+
+
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(canvas.width - 20, canvas.height - 100);
+ctx.arc(canvas.width - 45, canvas.height - 100, 25, 0, angle(140), false);
+ctx.stroke();
+ctx.strokeStyle = '#000000';
+ctx.closePath();
